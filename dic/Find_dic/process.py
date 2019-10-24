e3bbd59f7f845dcbfc2dd432cccbc847b8c4212d
trankimsen <trankimsen1681998@gmail.com>
2019-10-24 12:18:15 +0700

    add view

--- dic/Find_dic/process.py
+++ dic/Find_dic/process.py
@@ -1,23 +1,27 @@
 from typing import Tuple
-
+from django.conf import settings
+import json
 
 class TrieNode(object):    
     def __init__(self, char: str):
-        self.char = char # current charater 
+        self.char = char
         self.children = []
         self.word_finished = False 
         self.counter = 1
         self.index = -1
+        self.recomend = []
 
 def add(root, word: str,index = -1):
     node = root
     for char in word:
         found_in_child = False
+        node.recomend.append(index)
         for child in node.children:
             if child.char == char:
                 child.counter += 1
                 node = child
                 found_in_child = True
+                
                 break
         if not found_in_child:
             new_node = TrieNode(char)
@@ -29,7 +33,9 @@ def add(root, word: str,index = -1):
 def find_prefix(root, prefix: str):
     node = root
     if not root.children:
-        return False, 0
+        return -1
+    if not prefix:
+        return -1
     for char in prefix:
         char_not_found = True
         for child in node.children:
@@ -39,11 +45,49 @@ def find_prefix(root, prefix: str):
                 break
         if char_not_found:
             return -1
+            #return node.recomend
     return node.index
-def test(x):
+def test(find_name):
+    """
+        load data
+    """
+    try:
+        with open('find_dic/data.json') as json_dataset:
+            dataset = json.load(json_dataset)
+    except FileNotFoundError:
+        json_data = {"test": []}
+    """
+        process
+    """
     root = TrieNode('*')
-    add(root, "hackathon",1)
-    add(root, "hack",2)
+    for x in dataset:
+        add(root,dataset[x]["name"],x)
+    idx = None
+    idx = find_prefix(root,find_name)
+
+    # if len(idx)== 0:
+    #     return {
+    #         "name":"not found",
+    #         "mean":"not found"
+    #     }
+    # dem = 0 
+    # result = {}
+    # for x in idx:
+    #     print("toi la x trong for ",x)
+    #     result[dem] = {
+    #         "name":dataset[x]["name"],
+    #         "mean":dataset[x]["mean"]
+    #     }
+    #     dem = dem + 1
+    # print("toi la kq")
+    # print(result)
+    # return result
+    if idx == -1:
+        return {
+            "name":find_name,
+            "mean":"not found"
+        }
     return {
-        "id":find_prefix(root,x)
+        "name":dataset[idx]["name"],
+        "mean":dataset[idx]["mean"]
     }
